29c6328226bb21ef06d0ab17d489ff02d2d18e81
CONDOR425 <CONDOR.ST4@GMAIL.COM>
2025-01-25 16:44:36 +0100

    sistema di inseguimento

--- Assets/codici/AInavigatio.cs
+++ Assets/codici/AInavigatio.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AInavigatio : MonoBehaviour
 {
+    
+    NavMeshAgent agent;
+
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.position;
+
     }
 }
